3fb20673c55bae5c59bcb6f81f02c25a51838477
Fatsack <fatsack51@gmail.com>
2023-11-30 22:31:36 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: [
-    './dist/*.html',
-    './dist/**/*.html'
-  ],
-  whitelist: [
-    'body',
-    'html',
-    'h1',
-    'h2',
-    'h3',
-    'p',
-    'a',
-    'blockquote',
-    'intro',
-    'mode-dark'
-  ],
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 const cssnano = require('cssnano')({
   preset: ['default', {
     discardComments: {
@@ -32,7 +10,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production' ? [purgecss,  cssnano] : [],
-    ...process.env.NODE_ENV === 'admin' ? [cssnano] : []
+    ...process.env.TAILWIND_ENV === 'production' ? [cssnano] : []
   ],
 };
